ea345aea6a7de5891740580aebb8289a650adced
Annarchive <ytpt@mail.ru>
2022-06-24 17:08:29 +0400

    Lesson 81 - reselect part 1

--- src/Users/UsersContainer.jsx
+++ src/Users/UsersContainer.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { follow, setCurrentPage, unfollow, toggleFollowingProgress, getUsers } from '../redux/users-reducer';
+import { follow, setCurrentPage, unfollow, toggleFollowingProgress} from '../redux/users-reducer';
 import Users from './Users';
 import Preloader from '../components/common/Preloader/Preloader';
 import { compose } from 'redux';
+import {
+    getCurrentPage,
+    getFollowingInProgress,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount,
+    getUsers
+} from "../redux/users-selectors";
 
 class UsersContainer extends React.Component {
 
@@ -33,14 +41,25 @@ class UsersContainer extends React.Component {
     }
 }
 
+// let mapStateToProps = (state) => {
+//     return {
+//         users: state.usersPage.users,
+//         pageSize: state.usersPage.pageSize,
+//         totalUsersCount: state.usersPage.totalUsersCount,
+//         currentPage: state.usersPage.currentPage,
+//         isFetching: state.usersPage.isFetching,
+//         followingInProgress: state.usersPage.followingInProgress
+//     }
+// }
+
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state)
     }
 }
 
