a414b07507155cac08472034d50629a63da76b3a
Annarchive <ytpt@mail.ru>
2022-06-29 14:08:25 +0400

    Lesson 87 - refactor MyPosts to functional component again; addReact.memo()

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,9 +1,9 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
-import { Field, reduxForm } from 'redux-form';
-import { maxLengthCreator, required } from '../../../utils/validators/validators';
-import { Textarea } from '../../common/FormsControls/FormsControls';
+import {Field, reduxForm} from 'redux-form';
+import {maxLengthCreator, required} from '../../../utils/validators/validators';
+import {Textarea} from '../../common/FormsControls/FormsControls';
 
 const maxLength10 = maxLengthCreator(10);
 
@@ -24,35 +24,28 @@ const AddNewPostForm = (props) => {
 
 let AddNewPostFormRedux = reduxForm({form: 'ProfileAddNewPostForm'})(AddNewPostForm);
 
-class MyPosts extends PureComponent {
+const MyPosts = React.memo(props => {
+    let postsElements =
+        props.posts.map(p =>
+            <Post message={p.message} like={p.likesCount}/>);
 
-    // shouldComponentUpdate(nextProps, nextState) {
-    //     return nextProps != this.props || nextState != this.state;
-    // }
+    let newPostElement = React.createRef();
 
-    render() {
-        let postsElements =
-            this.props.posts.map(p =>
-                <Post message={p.message} like={p.likesCount}/>);
-
-        let newPostElement = React.createRef();
-
-        let onAddPost = (values) => {
-            this.props.addPost(values.newPostText);
-        }
-
-        return (
-            <div className={classes.postsBlock}>
-                <h3>My posts</h3>
-                <div>
-                    <AddNewPostFormRedux onSubmit={onAddPost}/>
-                </div>
-                <div className={classes.posts}>
-                    {postsElements}
-                </div>
-            </div>
-        )
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
     }
-}
+
+    return (
+        <div className={classes.postsBlock}>
+            <h3>My posts</h3>
+            <div>
+                <AddNewPostFormRedux onSubmit={onAddPost}/>
+            </div>
+            <div className={classes.posts}>
+                {postsElements}
+            </div>
+        </div>
+    )
+})
 
 export default MyPosts;
