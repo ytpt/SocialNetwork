6ed89b98b485f124bc11ae6edb55c80d03578ffd
Annarchive <ytpt@mail.ru>
2022-06-30 22:15:43 +0400

    Lesson 89 - Testing (jest, tdd); refactor deletePost case

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -48,8 +48,7 @@ const profileReducer = (state = initialState, action) => {
         case DELETE_POST: {
             return {
                 ...state,
-                posts: state.posts.filter(p => p.id != action.postId
-                }
+                posts: state.posts.filter(p => p.id != action.postId)
             }
         }
 
