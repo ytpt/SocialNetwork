9783c736c93c00fc330132af8d83d88e66f04a51
Annarchive <ytpt@mail.ru>
2022-06-30 20:21:58 +0400

    Lesson 89 - Testing (jest, tdd); relocate <BrowserRouter> <Provider> store={store} from Index.js to App.js (into return());

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-import store from './redux/redux-store';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Provider store={store}>
       <App />
-    </Provider>
-    </BrowserRouter>
   </React.StrictMode>, document.getElementById('root'));
 
 
