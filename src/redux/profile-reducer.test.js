74250e2adc62a18945ff69e96bddc769d6971330
Annarchive <ytpt@mail.ru>
2022-06-30 20:46:38 +0400

    Lesson 89 - Testing (jest, tdd); relocate let state under import; add the second test

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,19 +1,28 @@
 import profileReducer, {addPostActionCreator} from "./profile-reducer";
 
+let state = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: '15'},
+        {id: 2, message: 'It`s my first post', likesCount: '20'},
+        {id: 3, message: 'I`m sick and tired', likesCount: '2000'},
+        {id: 4, message: 'STOP WAR', likesCount: '1000000'}
+    ]
+}
+
 test('length of posts should be incremented', () => {
     //1) Готовим исходные данные
     let action = addPostActionCreator('Annarchive');
-    let state = {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: '15'},
-            {id: 2, message: 'It`s my first post', likesCount: '20'},
-            {id: 3, message: 'I`m sick and tired', likesCount: '2000'},
-            {id: 4, message: 'STOP WAR', likesCount: '1000000'}
-        ]
-    }
     //2) Action
     let newState = profileReducer(state, action);
-
     //3) Проверка ожиданий
     expect(newState.posts.length).toBe(5);
+});
+
+test('message of new post should be correct', () => {
+    //1) Готовим исходные данные
+    let action = addPostActionCreator('Annarchive');
+    //2) Action
+    let newState = profileReducer(state, action);
+    //3) Проверка ожиданий
+    expect(newState.posts[4].message).toBe('Annarchive');
 });
