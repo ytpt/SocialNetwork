ea5bc3401b19f52e1678e8189a8322e5e9a99795
Annarchive <ytpt@mail.ru>
2022-07-04 17:57:17 +0400

    Lesson 90 - Refactoring, redux-ducks; change getAuthUserData from response.then to async await function in auth-reducer.js

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -28,14 +28,13 @@ export const setAuthUserData = (userId, email, login, isAuth) => (
     {type: SET_USER_DATA, payload: {userId, email, login, isAuth}}
 );
 
-export const getAuthUserData = () => (dispatch) => {
-    return authAPI.me()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                let {id, email, login} = response.data.data;
-                dispatch(setAuthUserData(id, email, login, true));
-            }
-        })
+export const getAuthUserData = () => async (dispatch) => {
+    let response = await authAPI.me();
+
+    if (response.data.resultCode === 0) {
+        let {id, email, login} = response.data.data;
+        dispatch(setAuthUserData(id, email, login, true));
+    }
 }
 
 export const login = (email, password, rememberMe) => (dispatch) => {
