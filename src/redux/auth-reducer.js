a04b943ecad283acec132c77aa052b30ee100fb9
Annarchive <ytpt@mail.ru>
2022-07-04 21:07:15 +0400

    Lesson 90 - Refactoring, redux-ducks; change login and logout functions from response.then to async await function in auth-reducer.js

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -37,29 +37,26 @@ export const getAuthUserData = () => async (dispatch) => {
     }
 }
 
-export const login = (email, password, rememberMe) => (dispatch) => {
+export const login = (email, password, rememberMe) => async (dispatch) => {
 
-    authAPI.login(email, password, rememberMe)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-               dispatch(getAuthUserData())
-            } else {
-                let message = response.data.messages.length > 0
-                ? response.data.messages[0]
-                : 'Some error';
+    let response = await authAPI.login(email, password, rememberMe);
 
-                dispatch(stopSubmit('login', {_error: message}));
-            }
-        })
+    if (response.data.resultCode === 0) {
+       dispatch(getAuthUserData())
+    } else {
+        let message = response.data.messages.length > 0
+        ? response.data.messages[0] : 'Some error';
+
+        dispatch(stopSubmit('login', {_error: message}));
+    }
 }
 
-export const logout = () => (dispatch) => {
-    authAPI.logout()
-        .then(response => {
-            if (response.data.resultCode === 0) {
-               dispatch(setAuthUserData(null, null, null, false));
-            }
-        })
+export const logout = () => async (dispatch) => {
+    let response = await authAPI.logout();
+
+    if (response.data.resultCode === 0) {
+       dispatch(setAuthUserData(null, null, null, false));
+    }
 }
 
 export default authReducer;
