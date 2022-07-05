46a231fedc64ba9ab125a4c9f7c878fa91cc4e83
Annarchive <ytpt@mail.ru>
2022-07-05 17:29:50 +0400

    Lesson 90 - Refactoring, redux-ducks; refactor Users component  in Users.js by creating new component User in User.js file

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -1,43 +1,20 @@
-import react from 'react';
-import styles from './Users.module.css';
-import userPhoto from '../assets/images/user.png'
-import { NavLink } from 'react-router-dom';
+import React from 'react';
 import Paginator from "../components/common/Paginator/Paginator";
+import User from "./User";
 
 let Users = (totalUsersCount, pageSize, currentPage, onPageChanged, users, ...props) => {
     return <div>
         <Paginator currentPage={currentPage} onPageChanged={onPageChanged}
                    totalUsersCount={totalUsersCount} pageSize={pageSize} />
-    {
-        users.map(u => <div key={u.id}>
-            <span>
-                <div>
-                    <NavLink to={'/profile/' + u.id}>
-                        <img src={u.photos.small != null ? u.photos.small : userPhoto} 
-                            className={styles.userPhoto} />
-                    </NavLink>
-                </div>
-                <div>
-                    {u.followed 
-                        ? <button disabled={props.followingInProgress
-                            .some(id => id === u.id)} 
-                                onClick={ () => { props.unfollow(u.id) }}>
-                        Unfollow</button> 
-                        : <button disabled={props.followingInProgress
-                            .some(id => id === u.id)} 
-                                onClick={ () => { props.follow(u.id) }}>
-                        Follow</button>}
-                </div>
-            </span>
-            <span>
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-            </span>
-    </div>)
-    }
-</div>
+        {
+            users.map(u => <User
+                user={u}
+                key={u.id}
+                followingInProgress={props.followingInProgress}
+                unfollow={props.unfollow}
+                follow={props.follow} /> )
+        }
+    </div>
 }
 
 export default Users;
