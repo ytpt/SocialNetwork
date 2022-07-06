00f80304198d6fcc2d0b45441b2d36931876c3c5
Annarchive <ytpt@mail.ru>
2022-07-06 21:23:43 +0400

    Lesson 92 - Create new tests by react-test-renderer in ProfileStatus.test.jsx

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -4,15 +4,30 @@ import ProfileStatus from "./ProfileStatus";
 
 describe('ProfileStatus component', () => {
     test('Status from props should be in the state', () => {
-        const button = create(<ProfileStatus  status='it-kamasutra.com'/>);
+        const component = create(<ProfileStatus  status='it-kamasutra.com' />);
         const instance = component.getInstance();
         expect(instance.state.status).toBe('it-kamasutra.com');
     })
 
-    test('After creation span status should be displayed with correct status', () => {
-        const button = create(<ProfileStatus  status='it-kamasutra.com'/>);
-        const instance = component.getInstance();
-        let span = instance.findByType('span');
-        expect(span.length).toBe(1);
+    test('After creation span should be displayed', () => {
+        const component = create(<ProfileStatus  status='it-kamasutra.com' />);
+        const root = component.root;
+        let span = root.findByType('span');
+        expect(span.length).not.toBeNull(1);
+    })
+
+    test('After creation input shouldn`t be displayed', () => {
+        const component = create(<ProfileStatus  status='it-kamasutra.com' />);
+        const root = component.root;
+        expect(() => {
+            let input = root.findByType('input');
+        }).toThrow();
+    })
+
+    test('After creation span status should contains correct status', () => {
+    const component = create(<ProfileStatus  status='it-kamasutra.com' />);
+        const root = component.root;
+        let span = root.findByType('span');
+        expect(span.children[0]).toBe('it-kamasutra.com');
     })
 })
