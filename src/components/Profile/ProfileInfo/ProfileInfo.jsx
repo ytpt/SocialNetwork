cb96b20b6f0b181301cecf56988f8cc42bb5e1c4
Annarchive <ytpt@mail.ru>
2022-07-11 20:33:46 +0400

    Lesson 96 - refactor <img src={profile.photos.large || userPhoto} /> in ProfileInfo.jsx; import userPhoto from assets; add className={main.Photo} to img;

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 import Preloader from '../../common/Preloader/Preloader';
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from '../../../assets/images/user.png';
 
 const ProfileInfo = ({profile, status, updateStatus}) => {
   if (!profile) {
@@ -9,8 +10,9 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
   }
     return <div>
       <div className={classes.descriptionBlock}>
-        <img src={profile.photos.large != null
-            ? profile.photos.large : 'http://localhost:3000/img/user.png'} width='20%'/>
+        <img src={profile.photos.large || userPhoto} className={classes.mainPhoto} />
+        {/*<img src={profile.photos.large != null*/}
+        {/*    ? profile.photos.large : 'http://localhost:3000/img/user.png'} width='20%'/>*/}
         <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
       </div>
     </div> 
