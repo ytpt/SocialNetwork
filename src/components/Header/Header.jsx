d6d2ff03b4841ef7f0ebabae81fb2e7d00178811
Annarchive <ytpt@mail.ru>
2022-07-11 20:19:13 +0400

    refactor header img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './Header.module.css';
+import headerPhoto from '../../assets/images/logo.png'
 
 const Header = (props) => {
     return <header className={classes.header}>
-        <img src='http://localhost:3000/img/logo.png' />
+        <img src={headerPhoto} />
         <div className={classes.loginBlock}>
             { props.isAuth 
-                ? <div className={classes.myLogin}>{props.login} - <button onClick={props.logout} className={classes.loginBtn}>Logout</button></div>
+                ? <div className={classes.myLogin}>{props.login} -
+                    <button
+                        onClick={props.logout}
+                        className={classes.loginBtn}>Logout
+                    </button>
+                </div>
                 : <NavLink to={'/login/*'}>Login</NavLink> }
         </div>
     </header>
