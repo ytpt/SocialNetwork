5903d4e7f5c7a10f1cf10e86664e85cd1bbaef65
Annarchive <ytpt@mail.ru>
2022-07-11 22:16:30 +0400

    Lesson 96 - Add const SAVE_PHOTO_SUCCESS in profile-reducer.jsx; Add new case SAVE_PHOTO_SUCCESS to profileReducer in profile-reducer.jsx; Add export const savePhotoSucccess in profile-reducer.jsx; Add export default savePhoto in profile-reducer.jsx;

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Profile from './Profile';
-import { getUserProfile, updateStatus, getStatus } from '../../redux/profile-reducer'
+import {getUserProfile, updateStatus, getStatus, savePhoto} from '../../redux/profile-reducer'
 import { connect } from 'react-redux';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { compose } from 'redux';
