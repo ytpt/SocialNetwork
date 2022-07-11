7b633ffa2b00554ecf716a8265752f01cd922381
Annarchive <ytpt@mail.ru>
2022-07-11 22:39:31 +0400

    Lesson 96 - Add savePhoto(photoFile) to profileAPI in api.js; Add one more prop to Profile.jsx

--- src/api/api.js
+++ src/api/api.js
@@ -44,6 +44,16 @@ export const profileAPI = {
     updateStatus(status) {
         return instance.put(`profile/status`, { status: status })
 // /*Дописала */.then(response => response.data)
+    },
+
+    savephoto(photoFile) {
+        const formData = new FormData();
+        formData.append('image', photoFile)
+        return instance.put(`profile/photo`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
     }
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MyPostsContainer from './MyPosts/MyPostsContainer';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
+import {savePhoto} from "../../redux/profile-reducer";
 
 const Profile = (props) => {
     return <div>
         <ProfileInfo
+          savePhoto={props.savePhoto}
           isOwner={props.isOwner}
           profile={props.profile} 
           status={props.status} 
