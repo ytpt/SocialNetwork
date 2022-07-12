cdbc3da74b90989f7e8c73f40c4f78a958345d8f
Annarchive <ytpt@mail.ru>
2022-07-12 23:27:29 +0400

    Lesson 97 - refactor saveProfile component in profile-reducer.js.jsx

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -95,11 +95,12 @@ export const savePhoto = (file) => async (dispatch) => {
     }
 }
 
-export const saveProfile = (profile) => async (dispatch) => {
-    let response = await profileAPI.saveProfile(profile);
-    debugger
-    if (response.data.resultCode === 0) {
+export const saveProfile = (profile) => async (dispatch, getState) => {
+    const userId = getState().auth.userId;
 
+    const response = await profileAPI.saveProfile(profile);
+    if (response.data.resultCode === 0) {
+        dispatch(getUserProfile(userId));
     }
 }
 
