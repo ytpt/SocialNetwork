064152eef7d60df7f626cb55602e0d2d65e243b7
Annarchive <ytpt@mail.ru>
2022-07-13 23:12:58 +0400

    Lesson 97 - refactor onSubmit component in ProfileInfo.jsx; add return string to saveProfile component into profile-reducer.js

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -20,8 +20,9 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto, savePro
   }
 
   const onSubmit = (formData) => {
-      saveProfile(formData);
-      //setEditMode(false);
+      saveProfile(formData).then( () => {
+          setEditMode(false);
+      })
   }
 
     return <div>
